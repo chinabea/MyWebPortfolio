0bb91afe65f09664e4ff456ec40bad45a3fad94d
China T. Bea <chibea@my.cspc.edu.ph>
2024-06-17 22:14:00 +0800

    add default index vue

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-screen bg-black text-white flex flex-col items-center relative">
+    <!-- Striped background -->
+    <div class="striped-background absolute inset-0"></div>
+    <!-- Content container -->
+    <div class="black-background relative z-10">
+      <Navbar />
+      <Hero />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar.vue';
+import Hero from '@/components/hero.vue';
+
+export default {
+  components: {
+    Navbar,
+    Hero
+  }
+}
+</script>
+
+<style scoped>
+
+.black-background {
+  width: 100%;
+}
+</style>
+
+
