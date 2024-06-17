c36fe2e69b0b676aa4b996b01b9063eae499516d
China T. Bea <chibea@my.cspc.edu.ph>
2024-06-17 22:20:40 +0800

    feat: add components navitem

---
+++ components/navitem.vue
@@ -0,0 +1,11 @@
+<template>
+  <li><a href="#" class="hover:text-pink-500">{{ name }}</a></li>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{ name: string }>()
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
