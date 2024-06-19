fee70129f091d224e0db4aff52f0bb28954cf566
China T. Bea <chibea@my.cspc.edu.ph>
2024-06-19 18:57:52 +0800

    refactor: index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="min-h-screen bg-black text-white flex flex-col items-center relative">
-    <!-- Striped background -->
-    <div class="striped-background absolute inset-0"></div>
-    <!-- Content container -->
     <div class="black-background relative z-10">
       <Navbar />
       <Hero />
