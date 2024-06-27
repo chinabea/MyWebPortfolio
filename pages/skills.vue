b51e2b7df66a5d22a13ae0dda381e09e6c488a8d
China T. Bea <chibea@my.cspc.edu.ph>
2024-06-27 20:53:35 +0800

    feat: skills

---
+++ pages/skills.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="min-h-screen bg-black text-white flex flex-col items-center relative">
+    <div class="black-background relative z-10">
+      <Navbar />
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar.vue';
+
+
+export default {
+  components: {
+    Navbar,
+  }
+}
+</script>
+
+<style scoped>
+
+.black-background {
+  width: 100%;
+}
+</style>
+
+
