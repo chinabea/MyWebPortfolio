28a366509036221fa9f3dc9e843a477a51ade624
China T. Bea <chibea@my.cspc.edu.ph>
2024-07-03 20:41:11 +0800

    chore: update nuxt configuration file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-// Define the path to the config file
-// const configPath = path.resolve(__dirname, 'config.js');
-
-// Check if the config file exists
-// if (!fs.existsSync(configPath)) {
-//   throw new Error(`Resource not found!`);
-// }
-// Load the config file  
-// const customConfig = require(configPath);
-
 // Nuxt configuration
 export default defineNuxtConfig({
   ssr: true,
@@ -24,9 +14,4 @@ export default defineNuxtConfig({
   },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/css/main.css"],
-  // runtimeConfig: {
-  //   public: {
-  //     ...customConfig
-  //   }
-  // }
 });
