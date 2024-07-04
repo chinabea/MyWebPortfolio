97fe57d42ea5228319e6eefb4b4f9e446af5b368
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-07-04 21:34:13 +0800

    fix: replace `require()` with import from a static asset
    Don't use require(). It is not required.

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,31 +1,43 @@
 <template>
-  <div class="flex flex-wrap gap-4">
-    <CardComponent
-      title="The simplest example is kafka + golang"
-      description="This article presents a simple way to implement a micro-service architecture using Kafka, Golang and Docker"
-      :imageSource="require('@/assets/images/projects/RPIMTS-Tracking.png')"
-    />
-    <CardComponent
-      title="Another project title"
-      description="This article discusses another interesting topic relevant to your projects"
-      :imageSource="require('@/assets/images/projects/RPIMTS-Tracking.png')"
-    />
-    <CardComponent
-      title="Yet another project title"
-      description="This article introduces yet another fascinating project idea for you to explore"
-      :imageSource="require('@/assets/images/projects/RPIMTS-Tracking.png')"
-    />
-  </div>
+    <div class="flex flex-wrap gap-4">
+        <CardComponent
+            v-for="(project, index) in projects"
+            :key="index"
+            :title="project.title"
+            :description="project.description"
+            :imageSource="projectImage"
+        />
+    </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import CardComponent from './CardComponent.vue';
+import projectImage from '@/assets/images/projects/RPIMTS-Tracking.png';
 
-defineComponent({
-  components: {
-    CardComponent,
-  },
+export default defineComponent({
+    components: {
+        CardComponent,
+    },
+    data() {
+        return {
+            projectImage,
+            projects: [
+                {
+                    title: "The simplest example is kafka + golang",
+                    description: "This article presents a simple way to implement a micro-service architecture using Kafka, Golang and Docker"
+                },
+                {
+                    title: "The simplest example is kafka + golang",
+                    description: "This article presents a simple way to implement a micro-service architecture using Kafka, Golang and Docker"
+                },
+                {
+                    title: "The simplest example is kafka + golang",
+                    description: "This article presents a simple way to implement a micro-service architecture using Kafka, Golang and Docker"
+                },
+            ],
+        };
+    },
 });
 </script>
 
