52479bb3d555ac043a393d56939320e0aa7395ed
China T. Bea <chibea@my.cspc.edu.ph>
2024-07-05 14:10:45 +0800

    update navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -30,12 +30,11 @@
 
       <!-- Navigation links -->
       <ul :class="{ 'hidden': !isMenuOpen }" class="flex items-center space-x-6 ml-auto lg:flex">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/projects">Projects</NuxtLink></li>
-        <li><NuxtLink to="/experience">Experience</NuxtLink></li>
-        <li><NuxtLink to="/certificate">Certificate</NuxtLink></li>
-        <li><NuxtLink to="/skills">Skills</NuxtLink></li>
+        <li><NuxtLink to="/" exact-active-class="active-link">Home</NuxtLink></li>
+        <li><NuxtLink to="/about" exact-active-class="active-link">About</NuxtLink></li>
+        <li><NuxtLink to="/projects" exact-active-class="active-link">Projects</NuxtLink></li>
+        <li><NuxtLink to="/certificate" exact-active-class="active-link">Certificates</NuxtLink></li>
+        <li><NuxtLink to="/skills" exact-active-class="active-link">Skills</NuxtLink></li>
         <li>
           <a href="mailto:china.t.bea@gmail.com" class="bg-pink-500 text-white py-2 px-4 rounded-full block text-center" target="_blank">
             Contact Me
@@ -59,5 +58,8 @@ export default {
 <style scoped>
 .shadow-xl {
   box-shadow: 0 8px 100px -4px rgba(234, 111, 132, 0.8); /* Pink shadow with lighter shade */
+}
+.active-link {
+  @apply text-pink-500 font-bold;
 }
 </style>
